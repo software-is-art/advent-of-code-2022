e03ddc8420ff5c290e9056c6d1a86bc4198ac2e3
software-is-art <37229146+software-is-art@users.noreply.github.com>
2022-12-21 19:11:31 +1300

    * Day 12 matrix shift

--- day/12/solution_a.jl
+++ day/12/solution_a.jl
@@ -20,7 +20,44 @@ function readMap(filename)
     return (map, start, dest, dimensions)
 end
 
+abstract type Direction end
+struct Left <: Direction val::Int end
+struct Right <: Direction val::Int end
+struct Up <: Direction val::Int end
+struct Down <: Direction val::Int end
+
+function shift(input::AbstractMatrix, fillVal::Int, x::Int, y::Int)
+    m, n = size(input)
+    result = input
+    if x != 0
+        column = fill(fillVal, m, abs(x))
+        result = (x > 0 ? [column result[:, 1:(n - x)]] : [result[:, (abs(x) + 1):n] column])
+    end
+    if y != 0
+        column = fill(fillVal, abs(y), n)
+        result = (y > 0 ? [result[(y + 1):m, :]; column] : [column; result[1:(m - abs(y)), :]])
+    end
+    return result
+end
+
+function shift(input::AbstractMatrix, left::Left)
+    return shift(input, 100, abs(left.val) * -1, 0)
+end
+
+function shift(input::AbstractMatrix, left::Right)
+    return shift(input, 100, abs(left.val), 0)
+end
+
+function shift(input::AbstractMatrix, left::Up)
+    return shift(input, 100, 0, abs(left.val) * -1)
+end
+
+function shift(input::AbstractMatrix, left::Down)
+    return shift(input, 100, 0, abs(left.val))
+end
 
 (map, start, dest, dimensions) = readMap("day/12/input.txt")
 matrix = reshape(map, dimensions)
-println(matrix)
+right = shift(matrix, Right(1))
+grad = right - matrix
+println(grad)

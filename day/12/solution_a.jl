3006e7ff76d455dbd24d61370deaba60a758f899
software-is-art <37229146+software-is-art@users.noreply.github.com>
2023-01-01 13:13:29 +1300

    * Finished Day 12 part 1

--- day/12/solution_a.jl
+++ day/12/solution_a.jl
@@ -1,5 +1,3 @@
-using Test
-
 abstract type Direction end
 struct Left <: Direction end
 struct Right <: Direction end
@@ -7,77 +5,26 @@ struct Up <: Direction end
 struct Down <: Direction end
 const directions = [Left(), Right(), Up(), Down()]
 struct Point
-    x::UInt8
-    y::UInt8
+    x::Int
+    y::Int
 end
 
-function Point(x::Int64, y::Int64, bounds::Matrix{Int})
+function Point(x::Int, y::Int, bounds::Matrix{Int})
     if !checkbounds(Bool, bounds, x, y)
         return nothing
     end
-    return Point(convert(UInt8, x), convert(UInt8, y))
+    return Point(x, y)
 end
 
-function Point(x::UInt8, y::UInt8, bounds::Matrix{Int})
-    return Point(Int64(x), Int64(y), bounds)
-end
-
-function Point(tuple::Tuple{Int64, Int64}, bounds::Matrix{Int})
+function Point(tuple::Tuple{Int, Int}, bounds::Matrix{Int})
     (x, y) = tuple
     return Point(x, y, bounds)
 end
 
-struct Path
-    head::Point
-    parent::Union{Path, Nothing}
-end
-
-function Path(head::Point)
-    return Path(head, nothing)
-end
-
-function Base.iterate(path::Path)
-    return (path.head, path.parent)
-end
-
-function Base.iterate(::Path, next::Path)
-    return (next.head, next.parent)
-end
-
-function Base.iterate(::Path, next::Nothing)
-    return next
-end
-
-Base.IteratorSize(::Path) = Base.SizeUnknown()
-
-function pathLength(path::Path)
-    return reduce(+, [1 for _ in path])
-end
-
-function head(path::Path)
-    return path.head
-end
-
-struct Paths
+struct Graph
     map::Matrix{Int}
     start::Point
     dest::Point
-    forwards::Set{Path}
-    forwardsPoints::Set{Point}
-    backwards::Set{Path}
-    backwardsPoints::Set{Point}
-end
-
-function Paths(paths::Paths, forwards::Set{Path}, backwards::Set{Path})
-    return Paths(paths.map, paths.start, paths.dest, forwards, paths.forwardsPoints, backwards, paths.backwardsPoints)
-end
-
-function Paths(map::Matrix{Int}, start::Point, dest::Point)
-    forwards = Set{Path}()
-    backwards = Set{Path}()
-    push!(forwards, Path(start))
-    push!(backwards, Path(dest))
-    return Paths(map, start, dest, forwards, Set([start]), backwards, Set([dest]))
 end
 
 function readMap(filename)
@@ -102,7 +49,7 @@ function readMap(filename)
     (x, y) = dimensions
     reshaped = reshape(map, y, x)
     matrix = permutedims(reshaped)
-    return (matrix, Point(start, matrix), Point(dest, matrix))
+    return Graph(matrix, Point(start, matrix), Point(dest, matrix))
 end
 
 function coordinates(point::Point, bounds::Matrix{Int})
@@ -110,95 +57,49 @@ function coordinates(point::Point, bounds::Matrix{Int})
 end
 
 function move(direction::Up, point::Point, bounds::Matrix{Int})
-    return Point(point.x, point.y - UInt8(1), bounds)
+    return Point(point.x, point.y - 1, bounds)
 end
 
 function move(direction::Down, point::Point, bounds::Matrix{Int})
-    return Point(point.x, point.y + UInt8(1), bounds)
+    return Point(point.x, point.y + 1, bounds)
 end
 
 function move(direction::Left, point::Point, bounds::Matrix{Int})
-    return Point(point.x - UInt8(1), point.y, bounds)
+    return Point(point.x - 1, point.y, bounds)
 end
 
 function move(direction::Right, point::Point, bounds::Matrix{Int})
-    return Point(point.x + UInt8(1), point.y, bounds)
+    return Point(point.x + 1, point.y, bounds)
 end
 
-function push(items::Vector{Point}, item::Point)
-    new = copy(items)
-    push!(new, item)
-    return new
-end
-
-function fork(parent::Path, map::Matrix{Int}, next::Set{Path}, points::Set{Point})
+function visitNeighbours(current::Point, weight::Int, unvisited::Dict{Point, Int}, visited::Dict{Point, Int}, map::Matrix{Int})
     for dir in directions
-        h = head(parent)
-        target = move(dir, h, map)
-        if target === nothing
+        next = move(dir, current, map)
+        if next === nothing
             continue
         end
-        gradient = abs(map[target.x, target.y] - map[h.x, h.y])
-        if 0 <= gradient <= 1 && !in(target, parent)
-            push!(next, Path(target, parent))
-            push!(points, target)
+        gradient = map[next.x, next.y] - map[current.x, current.y]
+        if (gradient < 0 || 0 <= gradient <= 1) && haskey(unvisited, next)
+            unvisited[next] = min(unvisited[next], weight + 1)
         end
     end
+    delete!(unvisited, current)
+    visited[current] = weight
 end
 
-function traverse(paths::Set{Path}, map::Matrix{Int}, points::Set{Point})
-    next = Set{Path}()
-    for path in paths
-        fork(path, map, next, points)
+function traverse(graph::Graph)
+    (xDim, yDim) = size(graph.map)
+    unvisited = Dict{Point, Int}([(Point(x, y), typemax(Int)) for x in 1:xDim for y in 1:yDim])
+    visited = Dict{Point, Int}()
+    unvisited[graph.start] = 0
+    (weight, current) = findmin(unvisited)
+    while weight < typemax(Int)
+        visitNeighbours(current, weight, unvisited, visited, graph.map)
+        (weight, current) = findmin(unvisited)
     end
-    return next
+    return visited[graph.dest]
 end
 
-function converge(paths::Paths)
-    for forward in paths.forwards
-        for backward in paths.backwards
-            if head(forward) == head(backward)
-                return pathLength(forward) + pathLength(backward) - 2
-            end
-        end
-    end
-    return nothing
-end
-
-function traverseForwards(paths::Paths)
-    return Paths(
-        paths,
-        traverse(paths.forwards, paths.map, paths.forwardsPoints),
-        paths.backwards
-    )
-end
-
-function traverseBackwards(paths::Paths)
-    return Paths(
-        paths,
-        paths.forwards,
-        traverse(paths.backwards, paths.map, paths.backwardsPoints),
-    )
-end
-
-function traverse(paths::Paths)
-    while true 
-        if length(paths.forwards) < length(paths.backwards)
-            paths = traverseForwards(paths)
-        else
-            paths = traverseBackwards(paths)
-        end
-        println((length(paths.forwards), length(paths.backwards)))
-        if length(intersect(paths.forwardsPoints, paths.backwardsPoints)) > 0
-            println("Checking convergence")
-            converged = converge(paths)
-            if (converged !== nothing)
-                return converged
-            end
-        end
-    end
-end
-
-(map, start, dest) = readMap("day/12/input.txt")
-shortest = traverse(Paths(map, start, dest))
-println(shortest)
+graph = readMap("day/12/input.txt")
+distance = traverse(graph)
+println(distance)

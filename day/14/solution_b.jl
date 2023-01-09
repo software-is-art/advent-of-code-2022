6eb24b5dbc4768194e033bf29a12351933441cbf
software-is-art <37229146+software-is-art@users.noreply.github.com>
2023-01-09 20:27:00 +1300

    * Day 14 part 2

---
+++ day/14/solution_b.jl
@@ -0,0 +1,140 @@
+const Point = Tuple{Int, Int}
+
+abstract type Tile end
+struct Air <: Tile end
+struct Rock <: Tile end
+struct SandFalling <: Tile end
+struct SandAtRest <: Tile end
+
+abstract type Result end
+struct Done <: Result end
+struct OutOfBounds <: Result end
+struct Continue <: Result
+    point::Point
+end
+
+function step(point::Point, delta::Point)
+    (x, y) = point
+    (dx, dy) = delta
+    if dy != 0
+        s = div(dy, abs(dy))
+        (x, y) = (x, y + s)
+        (dx, dy) = (dx, dy - s)
+    end
+    if dx != 0
+        s = div(dx, abs(dx))
+        (x, y) = (x + s, y)
+        (dx, dy) = (dx - s, dy)
+    end
+    return ((x, y), (dx, dy))
+end
+
+function readInput(filename)
+    tiles = Dict{Point, Tile}()
+    for trace in readlines(filename)
+        previous = nothing
+        for span in split(trace, " -> ")
+            point = eval(Meta.parse(string("($span)")))
+            if previous !== nothing
+                delta = (previous[1] - point[1], previous[2] - point[2])
+                tiles[point] = Rock()
+                current = point
+                while delta != (0, 0)
+                    (current, delta) = step(current, delta)
+                    tiles[current] = Rock()
+                end
+            end
+            previous = point
+        end
+    end
+    return tiles
+end
+
+function getTile(slice::Dict{Point, Tile}, yBounds::Point, point::Point)
+    (_, yMax) = yBounds
+    (_, y) = point
+    if y == yMax + 2
+        return Rock()
+    end
+    if haskey(slice, point)
+        return slice[point]
+    end
+    return Air()
+end
+
+function xRange(slice::Dict{Point, Tile})
+    maxX = typemin(Int)
+    minX = typemax(Int)
+    for ((next, _), _) in slice
+        if next > maxX
+            maxX = next
+        end
+        if next < minX
+            minX = next
+        end
+    end
+    return (minX, maxX)
+end
+
+function yRange(slice::Dict{Point, Tile})
+    y = typemin(Int)
+    for ((_, next), _) in slice
+        if next > y
+            y = next
+        end
+    end
+    return (0, y)
+end
+
+function blocked(::Air)
+    return false
+end
+
+function blocked(next)
+    return true
+end
+
+function trickle(slice::Dict{Point, Tile}, previous::Continue, yBounds::Point)
+    (x, y) = previous.point
+    if blocked(getTile(slice, yBounds, previous.point))
+        return OutOfBounds()
+    end
+    nextPoint = (x, y + 1)
+    nextTile = getTile(slice, yBounds, nextPoint)
+    if (!blocked(nextTile))
+        return Continue(nextPoint)
+    end
+    nextPoint = (x - 1, y + 1)
+    nextTile = getTile(slice, yBounds, nextPoint)
+    if !blocked(nextTile)
+        return Continue(nextPoint)
+    end
+    nextPoint = (x + 1, y + 1)
+    nextTile = getTile(slice, yBounds, nextPoint)
+    if !blocked(nextTile)
+        return Continue(nextPoint)
+    end
+    slice[previous.point] = SandAtRest()
+    return Done()
+end
+
+function trickleSand(slice::Dict{Point, Tile})
+    source = (500, 0)
+    trickling = true
+    yBounds = yRange(slice)
+    while trickling
+        current = Continue(source)
+        while current !== Done() && (trickling = current !== OutOfBounds())
+            current = trickle(slice, current, yBounds)
+        end
+    end
+    return slice
+end
+
+function countSand(slice::Dict{Point, Tile})
+    return count(kvp -> kvp[2] === SandAtRest(), slice)
+end
+
+slice = trickleSand(readInput("day/14/input.txt"))
+sandCount = countSand(slice)
+println(sandCount)
